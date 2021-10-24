a1589bcc696abf2dce4540c34cda1398b95ab512
Marc Gravel <gravel.m@outlook.com>
2021-10-23 20:37:37 -0700

    initialized application

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-main>
+      <router-view :key="$route.fullPath"/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
+};
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 
-    &.router-link-exact-active {
-      color: #42b983;
+    * {
+        overflow-x: hidden;
+        margin: 0;
+        padding: 0;
+        font-family:  'Roboto', sans-serif;
     }
-  }
-}
+
 </style>
